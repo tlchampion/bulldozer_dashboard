c602738d3eb29488c737c9c48e76268fe0c47005
Thomas Champion <thomas@thomaschampion.net>
2022-02-15 13:41:08 -0800

    reduced number of observations in test set

--- sample.py
+++ sample.py
@@ -26,7 +26,7 @@ y_test = y_test[idx]
 choices = X_test.index.tolist()
 print(y_test.shape)
 
-idx2 = random.sample(choices, 1000)
+idx2 = random.sample(choices, 200)
 print(idx2)
 
 X_test = X_test[X_test.index.isin(idx2)]
